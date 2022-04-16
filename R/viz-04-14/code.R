37c6863d6e5a9241a3b75cb12896e63eb0b4d9be
= <=>
2022-04-16 11:53:32 -0300

    backup

--- R/viz-04-14/code.R
+++ R/viz-04-14/code.R
@@ -1,10 +1,14 @@
 # House prices and demographic trends #
 library(tidyverse)
 library(here)
+library(WDI)
+library(MetBrewer)
 library(showtext)
 font_add_google("Montserrat", "Montserrat")
 showtext_auto()
 source(here("R/theme.R"))
+
+colors <- met.brewer("Hokusai1", n = 8)[c(3, 7, 8)]
 
 # Import house price indexes from BIS #
 bis <- read_csv(here("data/2022_04/rppi_bis_data.csv"))
@@ -13,15 +17,19 @@ bis <- bis %>%
   filter(year >= 1960, unit == "Index, 2010 = 100")
 
 bis_real <- bis %>%
-  filter(nominal == "Real", month == 6) %>%
+  filter(nominal == "Real") %>%
+  select(year, country = reference_area, value) %>%
+  group_by(year, country) %>%
+  summarise(index_house = mean(value, na.rm = T))
+
+bis_nominal <- bis %>%
+  filter(nominal == "Nominal") %>%
   select(year, country = reference_area, value) %>%
   group_by(year, country) %>%
   summarise(index_house = mean(value, na.rm = T))
 
 # Import demographic data from WBD #
-library(WDI)
-search <- WDIsearch("population")
-
+#search <- WDIsearch("population")
 # Total population
 pop <- WDI(indicator = "SP.POP.TOTL")
 # Population growth
@@ -58,24 +66,26 @@ base_pop <- tbl_pop %>%
   filter(year == 2010) %>%
   select(country, base_pop = pop, base_pop_adult = pop_adult)
 
-# Join information across tables
-tbl <- bis_nominal %>%
-  filter(year >= 1970) %>%
-  left_join(tbl_pop) %>%
+tbl_pop <- tbl_pop %>%
   left_join(base_pop) %>%
   mutate(index_pop = pop / base_pop * 100,
-         index_pop_adult = pop_adult / base_pop_adult * 100) %>%
+         index_pop_adult = pop_adult / base_pop_adult * 100)
+
+# Join information across tables
+tbl <- bis_real %>%
+  filter(year >= 1970, year <= 2020) %>%
+  left_join(tbl_pop) %>%
+  select(year, country, index_house, index_pop, index_pop_adult) %>%
+  pivot_longer(cols = -c(year, country), names_to = "index")
+
+tbl_nominal <- bis_nominal %>%
+  filter(year >= 1970, year <= 2020) %>%
+  left_join(tbl_pop) %>%
   select(year, country, index_house, index_pop, index_pop_adult) %>%
   pivot_longer(cols = -c(year, country), names_to = "index")
 
 sel <- c("Australia", "United States", "Japan", "Germany", "Austria", "France")
-sel <- c("Chile", "Brazil", "Peru", "Mexico")
-
-ggplot(filter(tbl, country %in% sel),
-       aes(x = year, y = value, colour = index)) +
-  geom_line(size = 1) +
-  facet_wrap(~country) +
-  theme_vini
+sel_latam <- c("Chile", "Brazil", "Peru", "Mexico", "Colombia")
 
 ggplot(tbl,
        aes(x = year, y = value, colour = index)) +
@@ -83,3 +93,62 @@ ggplot(tbl,
   facet_wrap(~country) +
   guides(color = "none") +
   theme_vini
+
+
+sel_conforme <- c("Singapore", "Belgium", "France", "Japan", "Korea", "Italy")
+sel_acima <- c("Switzerland", "Hong Kong SAR", "Chile", "India", "Malaysia", "Estonia")
+
+ggplot(filter(tbl, country %in% sel_conforme, year >= 2000),
+       aes(x = year, y = value, colour = index)) +
+  geom_line(size = 1) +
+  geom_hline(yintercept = 100, linetype = 2, color = "gray60") +
+  facet_wrap(~country) +
+  scale_color_manual(
+    values = colors,
+    name = "",
+    labels = c("Real House Prices", "Population", "Adult Pop.")) +
+  scale_y_continuous(breaks = seq(60, 140, 20)) +
+  labs(title = "House prices follow population growth",
+       x = NULL,
+       y = "Index (2010 = 100)",
+       caption = "Source: Real House Price Indexes (BIS), Population (UN).\nAdult Pop. defined as population from 20-44 years.") +
+  theme_vini
+
+ggplot(filter(tbl, country %in% sel_acima, year >= 2000),
+       aes(x = year, y = value, colour = index)) +
+  geom_line(size = 1) +
+  geom_hline(yintercept = 100, linetype = 2, color = "gray60") +
+  facet_wrap(~country) +
+  scale_color_manual(
+    values = colors,
+    name = "",
+    labels = c("Real House Prices", "Population", "Adult Pop.")) +
+  scale_y_continuous(breaks = seq(60, 200, 20)) +
+  labs(title = "In some markets, hose prices have detached from pop. growth",
+       x = NULL,
+       y = "Index (2010 = 100)",
+       caption = "Source: Real House Price Indexes (BIS), Population (UN).\nAdult Pop. defined as population from 20-44 years.") +
+  theme_vini
+
+ggplot(filter(tbl, country %in% sel_latam, year >= 2000),
+       aes(x = year, y = value, colour = index)) +
+  geom_line(size = 1) +
+  geom_hline(yintercept = 100, linetype = 2, color = "gray60") +
+  facet_wrap(~country) +
+  scale_color_manual(
+    values = colors,
+    name = "",
+    labels = c("Real House Prices", "Population", "Adult Pop.")) +
+  scale_y_continuous(breaks = seq(60, 200, 20)) +
+  labs(title = "LATAM markets are not synched",
+       x = NULL,
+       y = "Index (2010 = 100)",
+       caption = "Source: Real House Price Indexes (BIS), Population (UN).\nAdult Pop. defined as population from 20-44 years.") +
+  theme_vini
+
+tbl %>%
+  filter(year %in% c(2000, 2020))
+
+
+
+
