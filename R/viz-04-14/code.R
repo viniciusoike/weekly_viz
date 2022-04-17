709c0dc89c1c7f62e190a7453a4c144f5391c58e
= <=>
2022-04-17 00:33:30 -0300

    Update code.R

--- R/viz-04-14/code.R
+++ R/viz-04-14/code.R
@@ -1,5 +1,6 @@
 # House prices and demographic trends #
 library(tidyverse)
+library(ggrepel)
 library(here)
 library(WDI)
 library(MetBrewer)
@@ -29,6 +30,9 @@ bis_nominal <- bis %>%
   summarise(index_house = mean(value, na.rm = T))
 
 # Import demographic data from WBD #
+
+wdi_names <- as_tibble(WDI_data$country)
+
 #search <- WDIsearch("population")
 # Total population
 pop <- WDI(indicator = "SP.POP.TOTL")
@@ -72,20 +76,55 @@ tbl_pop <- tbl_pop %>%
          index_pop_adult = pop_adult / base_pop_adult * 100)
 
 # Join information across tables
+
+# Real property prices
 tbl <- bis_real %>%
   filter(year >= 1970, year <= 2020) %>%
   left_join(tbl_pop) %>%
   select(year, country, index_house, index_pop, index_pop_adult) %>%
   pivot_longer(cols = -c(year, country), names_to = "index")
-
+# Nominal property prices
 tbl_nominal <- bis_nominal %>%
   filter(year >= 1970, year <= 2020) %>%
   left_join(tbl_pop) %>%
   select(year, country, index_house, index_pop, index_pop_adult) %>%
   pivot_longer(cols = -c(year, country), names_to = "index")
 
+# Convert real prices to wide and compare evolution
+tbl_wide <- tbl %>%
+  filter(year %in% c(2010, 2020),
+         index %in% c("index_house", "index_pop")) %>%
+  group_by(country, index) %>%
+  mutate(diff = diff(value)) %>%
+  ungroup() %>%
+  filter(year == 2020) %>%
+  pivot_wider(id_cols = country, names_from = index, values_from = diff) %>%
+  left_join(select(wdi_names, country, iso3c)) %>%
+  mutate(highlight = factor(if_else(country == "World", 1, 0)))
+
+tbl_wide_lr <- tbl_nominal %>%
+  filter(year %in% c(1990, 2020),
+         index %in% c("index_house", "index_pop")) %>%
+  group_by(country, index) %>%
+  mutate(diff = diff(value)) %>%
+  ungroup() %>%
+  filter(year == 2020, !is.na(diff)) %>%
+  pivot_wider(id_cols = country, names_from = index, values_from = diff) %>%
+  left_join(select(wdi_names, country, iso3c)) %>%
+  mutate(highlight = factor(if_else(country == "World", 1, 0)))
+
+
 sel <- c("Australia", "United States", "Japan", "Germany", "Austria", "France")
 sel_latam <- c("Chile", "Brazil", "Peru", "Mexico", "Colombia")
+
+ggplot(filter(tbl, country == "United States"),
+       aes(x = year, y = value, color = index)) +
+  geom_line(size = 1) +
+  scale_color_manual(
+    values = colors,
+    name = "",
+    labels = c("Real House Prices", "Population", "Adult Pop.")) +
+  theme_vini
 
 ggplot(tbl,
        aes(x = year, y = value, colour = index)) +
@@ -146,9 +185,24 @@ ggplot(filter(tbl, country %in% sel_latam, year >= 2000),
        caption = "Source: Real House Price Indexes (BIS), Population (UN).\nAdult Pop. defined as population from 20-44 years.") +
   theme_vini
 
-tbl %>%
-  filter(year %in% c(2000, 2020))
 
 
+ggplot(data = na.omit(tbl_wide),
+       aes(x = index_pop, y = index_house)) +
+  geom_hline(yintercept = 0) +
+  geom_vline(xintercept = 0) +
+  geom_abline(slope = 1, intercept = 0, linetype = 2, color = colors[1]) +
+  geom_point(aes(color = highlight)) +
+  geom_text_repel(aes(label = iso3c, color = highlight)) +
+  scale_x_continuous(breaks = seq(-10, 30, 5)) +
+  scale_y_continuous(breaks = seq(-30, 90, 15)) +
+  scale_color_manual(values = c("black", colors[1])) +
+  guides(color = "none") +
+  labs(
+    title = "Real House Prices x Population (2010/20)",
+    x = "Population (2010/2020)",
+    y = "RPPI (2010/2020)",
+    caption = "Source: Real House Price Indexes (BIS), Population (UN).") +
+  theme_vini
 
 
